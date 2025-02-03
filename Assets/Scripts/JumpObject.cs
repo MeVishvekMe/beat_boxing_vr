24a47d1f154395099c817e3e96ab3b599104e10a
Vishvek Methwani <methwani.vishvek09@gmail.com>
2025-02-03 15:15:57 +0530

    jump bug fixing

--- Assets/Scripts/JumpObject.cs
+++ Assets/Scripts/JumpObject.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class JumpObject : MonoBehaviour {
 
     private bool didJumped = false;
+    private float jumpThreshold = 0.12f;
 
     private void OnTriggerStay(Collider other) {
         if (other.CompareTag("Player")) {
-            if (Camera.main.transform.position.y >= PlayerHeight.PlayerHeightValue + 0.20f) {
+            if (Camera.main.transform.position.y >= PlayerHeight.PlayerHeightValue + jumpThreshold) {
                 didJumped = true;
             }
         }
